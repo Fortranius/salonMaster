baf94adc9a22b40678b5f511d1d394edba5223eb
fortranius <fortranius@bk.ru>
2018-12-01 23:26:28 +0300

    config web cors

--- src/main/java/ru/salon/repository/MasterRepository.java
+++ src/main/java/ru/salon/repository/MasterRepository.java
@@ -4,6 +4,8 @@ import ru.salon.model.Master;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MasterRepository extends JpaRepository<Master, Long> {
 }

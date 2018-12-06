a9af13424d34bfbebb13924b20d0a4b9d7877d3b
fortranius <fortranius@bk.ru>
2018-12-06 18:57:37 +0300

    create update client

--- src/main/java/ru/salon/controller/ClientController.java
+++ src/main/java/ru/salon/controller/ClientController.java
@@ -3,10 +3,12 @@ package ru.salon.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.salon.exception.ResourceNotFoundException;
 import ru.salon.model.Client;
 import ru.salon.repository.ClientRepository;
-import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -29,8 +31,18 @@ public class ClientController {
                 .orElseThrow(() -> new ResourceNotFoundException("Master not found with id " + clientId));
     }
 
+    @PostMapping("/client")
+    public Client createClient(@Valid @RequestBody Client client) {
+        return clientRepository.save(client);
+    }
+
+    @PutMapping("/client")
+    public Client updateClient(@RequestBody Client client) {
+        return clientRepository.save(client);
+    }
+
     @DeleteMapping("/client/{clientId}")
-    public ResponseEntity<?> deleteMAster(@PathVariable Long clientId) {
+    public ResponseEntity<?> deleteClint(@PathVariable Long clientId) {
         return clientRepository.findById(clientId)
                 .map(client -> {
                     clientRepository.delete(client);

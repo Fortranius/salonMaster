e3ff55b692562336d2c514697725d156e2b8bd1d
fortranius <fortranius@bk.ru>
2018-12-07 21:06:32 +0300

    create timeslot

--- src/main/java/ru/salon/controller/TimeSlotController.java
+++ src/main/java/ru/salon/controller/TimeSlotController.java
@@ -3,9 +3,13 @@ package ru.salon.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.salon.model.TimeSlot;
 import ru.salon.repository.TimeSlotRepository;
+
+import javax.validation.Valid;
 
 @RestController
 public class TimeSlotController {
@@ -19,6 +23,11 @@ public class TimeSlotController {
     @GetMapping("/timeSlots")
     public Page<TimeSlot> getTimeSlots(Pageable pageable) {
         return timeSlotRepository.findAll(pageable);
+    }
+
+    @PostMapping("/timeSlot")
+    public TimeSlot createTimeSlot(@Valid @RequestBody TimeSlot timeSlot) {
+        return timeSlotRepository.save(timeSlot);
     }
 
 }

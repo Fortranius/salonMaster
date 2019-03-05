e1566bd8e0b5e56ec5d4ac5c54852f185793dfc6
asaparov <BN+2yndD>
2019-03-05 16:41:01 +0300

    sales refactor

--- src/main/java/ru/salon/controller/SaleController.java
+++ src/main/java/ru/salon/controller/SaleController.java
@@ -1,6 +1,8 @@
 package ru.salon.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import ru.salon.model.Sale;
 import ru.salon.repository.SaleRepository;
@@ -13,6 +15,11 @@ import javax.validation.Valid;
 public class SaleController {
 
     private SaleRepository saleRepository;
+
+    @GetMapping("/sales")
+    public Page<Sale> getExpenses(Pageable pageable) {
+        return saleRepository.findAll(pageable);
+    }
 
     @PostMapping("/sale")
     public Sale createSale(@Valid @RequestBody Sale sale) {

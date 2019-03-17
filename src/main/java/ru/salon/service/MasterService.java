b1f94714411fcf2fd7014d34a27e1aeb8bb28231
fortranius <fortranius@bk.ru>
2019-03-17 13:24:41 +0300

    add Master to graph

--- src/main/java/ru/salon/service/MasterService.java
+++ src/main/java/ru/salon/service/MasterService.java
@@ -2,10 +2,14 @@ package ru.salon.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import ru.salon.model.Master;
+import ru.salon.model.TimeSlot;
 import ru.salon.repository.MasterRepository;
+import ru.salon.repository.TimeSlotRepository;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,8 +21,12 @@ public class MasterService {
 
     private MasterRepository masterRepository;
 
+    private TimeSlotRepository timeSlotRepository;
+
     public List<Master> getMastersByWorkDay(Instant day) {
         return masterRepository.findAll().stream().filter(master -> {
+            List<TimeSlot> timeSlots = timeSlotRepository.findByStartSlotBetweenAndMaster(day, day.plus(1, ChronoUnit.DAYS), master);
+            if (!CollectionUtils.isEmpty(timeSlots)) return true;
             long diff = DAYS.between(master.getStartDateWork(), day) + 1;
             if (diff % 7 == 0) return true;
             if (diff<=0) return false;

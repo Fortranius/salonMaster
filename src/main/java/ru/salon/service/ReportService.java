44356ccfb41f38b7b63b4e30bbda7f81f786901e
fortranius <fortranius@bk.ru>
2019-04-18 15:22:22 +0300

    static table

--- src/main/java/ru/salon/service/ReportService.java
+++ src/main/java/ru/salon/service/ReportService.java
@@ -33,21 +33,30 @@ public class ReportService {
     private AdditionalIncomeRepository additionalIncomeRepository;
 
     public StaticData getStatisticBetweenDate(Instant start, Instant end, Long masterId) {
-        Master master = masterRepository.findAll().get(0);
-        return generateArray(start, end, master);
+        return generateArray(start, end, masterRepository.findAll());
     }
 
-    private StaticData generateArray(Instant start, Instant end, Master master) {
-
-        List<TimeSlot> timeSlots = timeSlotRepository
-                .findByStartSlotBetweenAndMasterAndStatus(start, end, master, StatusOrder.DONE);
-        List<AdditionalIncome> additionalIncomes = additionalIncomeRepository
-                .findByDateBetweenAndMaster(start, end, master);
-        List<Expense> expenses = expenseRepository.findByDateBetweenAndMaster(start, end, master);
-
+    private StaticData generateArray(Instant start, Instant end, List<Master> masters) {
         JSONArray data = new JSONArray();
         JSONArray columns = new JSONArray();
-        addColumn(columns, "day", "Дата");
+        addColumn(columns, "day", "Дата", null, null);
+
+        masters.forEach(master -> {
+            List<Expense> expenses = expenseRepository.findByDateBetweenAndMaster(start, end, master);
+            Map<Product, Integer> sums = expenses.stream().filter(expense ->
+                    expense.getDate().isAfter(start) && expense.getDate().isBefore(end))
+                    .collect(Collectors.groupingBy(Expense::getProduct, Collectors.summingInt(Expense::getCountProduct)));
+
+            addColumn(columns, master.getId().toString(), master.getPerson().getName(), "3", "0");
+
+            addColumn(columns, "master" + master.getId() + ".sumIncome" + master.getId(), "Услуги", null, "1");
+            addColumn(columns, "master" + master.getId() + ".sumIncomeMaster" + master.getId(), "З/п", null, "1");
+
+            sums.forEach((product, integer) -> {
+                addColumn(columns, "master" + master.getId() + ".product" + master.getId() + product.getId(),
+                        product.getDescription(), null, "1");
+            });
+        });
 
         Instant currentDateStart = start;
         Instant currentDateEnd = start.plus(1, ChronoUnit.DAYS);
@@ -55,25 +64,33 @@ public class ReportService {
             JSONObject obj = new JSONObject();
             LocalDateTime date = LocalDateTime.ofInstant(currentDateStart, ZoneId.systemDefault());
             obj.put("day", date.getDayOfMonth() + " " + getMonth(date.getMonthValue()));
-            obj.put("master" + master.getId(), generateAllSum(currentDateStart,
-                    currentDateEnd,
-                    master,
-                    timeSlots,
-                    additionalIncomes,
-                    expenses,
-                    columns));
+
+            for (Master master:masters) {
+                List<TimeSlot> timeSlots = timeSlotRepository
+                        .findByStartSlotBetweenAndMasterAndStatus(start, end, master, StatusOrder.DONE);
+                List<AdditionalIncome> additionalIncomes = additionalIncomeRepository
+                        .findByDateBetweenAndMaster(start, end, master);
+                List<Expense> expenses = expenseRepository.findByDateBetweenAndMaster(start, end, master);
+
+                obj.put("master" + master.getId(), generateAllSum(currentDateStart,
+                        currentDateEnd,
+                        master,
+                        timeSlots,
+                        additionalIncomes,
+                        expenses));
+            }
             currentDateStart = currentDateStart.plus(1, ChronoUnit.DAYS);
             currentDateEnd = currentDateEnd.plus(1, ChronoUnit.DAYS);
             data.add(obj);
         }
+
         return StaticData.builder().columns(columns).data(data).build();
     }
 
     private JSONObject generateAllSum(Instant start, Instant end, Master master,
                                       List<TimeSlot> timeSlots,
                                       List<AdditionalIncome> additionalIncomes,
-                                      List<Expense> expenses,
-                                      JSONArray columns) {
+                                      List<Expense> expenses) {
 
         BigDecimal sumIncome = timeSlots.stream().filter(timeSlot ->
                 timeSlot.getStartSlot().isAfter(start) && timeSlot.getStartSlot().isBefore(end))
@@ -93,24 +110,22 @@ public class ReportService {
         masterObject.put("sumIncome" + master.getId(), sumIncome);
         masterObject.put("sumIncomeMaster" + master.getId(), sumIncomeMaster);
 
-        addColumn(columns, "master" + master.getId() + ".sumIncome" + master.getId(), "Услуги");
-        addColumn(columns, "master" + master.getId() + ".sumIncomeMaster" + master.getId(), "З/п");
-
         Map<Product, Integer> sums = expenses.stream().filter(expense ->
                 expense.getDate().isAfter(start) && expense.getDate().isBefore(end))
                 .collect(Collectors.groupingBy(Expense::getProduct, Collectors.summingInt(Expense::getCountProduct)));
 
         sums.forEach((product, integer) -> {
             masterObject.put("product" + master.getId() + product.getId(), integer);
-            addColumn(columns, "master" + master.getId() + ".product" + master.getId() + product.getId(), product.getDescription());
         });
         return masterObject;
     }
 
-    private void addColumn(JSONArray columns, String dataField, String text) {
+    private void addColumn(JSONArray columns, String dataField, String text, String colSpan, String row) {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("dataField", dataField);
+        if (colSpan == null) jsonObject.put("dataField", dataField);
         jsonObject.put("text", text);
+        if (colSpan != null) jsonObject.put("colSpan", colSpan);
+        if (row != null) jsonObject.put("row", row);
         columns.add(jsonObject);
     }
 }
